27b62adaf1fa4d52602c4bf9b1241413f9799575
LuchoDB <lucianodiazbertozzi@gmail.com>
2025-05-16 17:24:19 -0300

    Ejercicio2 - PromedioVarios

--- Guia7.3/Ejercicio1/Promedio20.cs
+++ Guia7.3/Ejercicio1/Promedio20.cs
@@ -6,10 +6,23 @@ using System.Threading.Tasks;
 
 namespace Ejercicio1
 {
-    internal class Program
+    internal class Promedio20
     {
         static void Main(string[] args)
         {
+            int suma = 0;
+            double cant = 0;
+
+            for (int i = 1 ; i<=20; i++)
+            {
+                Console.WriteLine($"Ingrese el numero {i}");
+                int num = Convert.ToInt32(Console.ReadLine());
+                suma += num;
+                cant++;
+                
+            }
+            double promedio = suma / cant;
+            Console.WriteLine($"El promedio de los 20 numeros es: {promedio}");
         }
     }
 }
---
+++ Guia7.3/Ejercicio2/PromedioVarios.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    internal class PromedioVarios
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese un num o pulse 0 para salir");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int suma = 0;
+            double cant = 0;
+
+            while (num!= 0)
+            {
+                suma += num;
+                cant++;
+                Console.WriteLine("Ingrese un num o pulse 0 para salir");
+            }
+
+            if (cant > 0)
+            {
+                double prom = suma / cant;
+                Console.WriteLine($"El promedio de los numero es {prom}");
+            }
+            else
+            {
+                Console.WriteLine("No se ingresaron numeros");
+            }
+
+        }
+    }
+}
